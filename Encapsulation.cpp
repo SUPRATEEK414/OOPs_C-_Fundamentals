8bcbb7c23c33d9c2f72d566fcc9070222a462f24
Suprateek Sarkar <81064383+SUPRATEEK414@users.noreply.github.com>
2024-05-29 10:41:04 +0530

    Add files via upload

---
+++ Encapsulation.cpp
@@ -0,0 +1,41 @@
+// #include <iostream>
+// using namespace std;
+// class Company{
+// private:
+// int salary;
+// public:
+// void setSalary(int overtime, int fulltime){
+//   salary = overtime + fulltime;
+// }
+// int getSalary(){
+//   return salary;
+// }
+// };
+// int main(){
+//   Company obj;
+//   obj.setSalary(30000, 60000);
+//   cout<<"Total Salary : "<<endl;
+//   cout<<obj.getSalary();
+//   return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// class Ball{
+// private:
+// float area;
+// public:
+// void setArea(float radius){
+//   area = 3.14 * radius * radius;
+// }
+// float getArea(){
+//   return area;
+// }
+// };
+// int main(){
+//   Ball obj;
+//   obj.setArea(6);
+//   cout<<"Area of ball is: "<<endl;
+//   cout<<obj.getArea();
+//   return 0;
+// }
