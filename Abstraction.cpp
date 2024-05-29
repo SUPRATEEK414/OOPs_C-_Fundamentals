09fcefba0018c905aff62414d5422b6604d3b857
Suprateek Sarkar <81064383+SUPRATEEK414@users.noreply.github.com>
2024-05-29 12:38:06 +0530

    Add files via upload

---
+++ Abstraction.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+// using namespace std;
+// class Car{
+//   bool startEngine;
+//   public:
+//   void start(){
+//   startEngine=true;
+//     cout<<"Engine started :)"<<endl;
+//   }
+//   void drive(){
+//     if(startEngine){
+//       cout<<"Your are ready to drive"<<endl;
+//     }
+//     else{
+//       cout<<"Can't drive the car"<<endl;
+//     }
+//   }
+// };
+// int main() {
+//   Car c;
+//   c.start();
+//   c.drive();
+//   return 0;
+// }
